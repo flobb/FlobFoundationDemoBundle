141bea3303d4bb99aadcba01beab15e5e97ac46e
Florian Belhomme <florian.belhomme@gmail.com>
2013-12-10 15:37:32 +0100

    Add the files

---
+++ Controller/DemoController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace FlorianBelhomme\Bundle\FoundationDemoBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DemoController extends Controller
+{
+    public function kitchensinkAction(Request $request)
+    {
+        $form = $this->createForm(new KitchensinkType());
+        
+        $form->bind(array(
+            'radio'             => 3,
+            'checkbox_disabled' => array(1)
+        ));
+        $form->addError(new FormError('This is a global form error message.'));
+        $form->addError(new FormError('This is another global form error message.'));
+        
+        return $this->render('FlorianBelhommeFoundationBundle:Demo:kitchensink.html.twig', array(
+            'form' => $form->createView()
+        ));
+    }
+}
---
+++ FlorianBelhommeFoudationDemoBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace FlorianBelhomme\Bundle\FoudationDemoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class FlorianBelhommeFoudationDemoBundle extends Bundle
+{
+}
