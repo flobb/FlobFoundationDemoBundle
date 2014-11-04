cdd8e938f5ca8d05cd59ba29ec941df096ca7386
Florian Belhomme <florian.belhomme@gmail.com>
2014-11-04 09:54:39 +0100

    WIP

--- Form/Type/KitchensinkType.php
+++ Form/Type/KitchensinkType.php
@@ -4,54 +4,14 @@ namespace Flob\Bundle\FoundationDemoBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolverInterface;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class KitchensinkType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
-    {https://github.com/symfony/symfony/pull/10272
-        // Text Fields
-        $builder->add('text',               'text',     array('label' => 'Default text', 'required' => false, 'read_only' => false));
-        $builder->add('textRequired',       'text',     array('label' => 'Required text', 'required' => true, 'read_only' => false));
-        $builder->add('textDisabled',       'text',     array('label' => 'Disabled text', 'required' => false, 'read_only' => true));
-        $builder->add('textError',          'text',     array('label' => 'Text with error', 'constraints' => array(new NotBlank(), new NotNull())));
-        $builder->add('email',              'email',    array('label' => 'Email'));
-        $builder->add('integer',            'integer',  array('label' => 'Integer'));
-        $builder->add('money',              'money',    array('label' => 'Money'));
-        $builder->add('number',             'number',   array('label' => 'Number'));
-        $builder->add('password',           'password', array('label' => 'Password'));
-        $builder->add('percent',            'percent',  array('label' => 'Percent'));
-        $builder->add('search',             'search',   array('label' => 'Search'));
-        $builder->add('url',                'url',      array('label' => 'Url'));
-
-        // Choices
-        $builder->add('radio',              'choice',   array('label' => 'Radio', 'choices' => array(1 => 'Choice 1', 2 => 'Choice 2', 3 => 'Obi wan kenobi'), 'multiple' => false, 'expanded' => true));
-        $builder->add('radio_disabled',     'choice',   array('label' => 'Radio disabled', 'choices' => array(1 => 'Yoda'), 'multiple' => false, 'expanded' => true, 'disabled' => true));
-        $builder->add('checkbox',           'choice',   array('label' => 'Checkbox', 'choices' => array(1 => 'Choice 1', 2 => 'Choice 2', 3 => 'Obi wan kenobi'), 'required' => true, 'multiple' => true, 'expanded' => true));
-        $builder->add('checkbox_disabled',  'choice',   array('label' => 'Checkbox disabled', 'choices' => array(1 => 'Yoda'), 'data' => array(1), 'multiple' => true, 'expanded' => true, 'disabled' => true));
-
-        // Slider
-        $builder->add('slider',             'slider',   array('label' => 'Slider', 'start' => 10, 'end' => 20, 'step' => 2));
-
-        // Switch
-        $builder->add('switch_radio',       'switch',   array('label' => 'Switch (as radio)', 'choices' => array(1 => 'Choice 1', 2 => 'Choice 2', 3 => 'Obi wan kenobi'), 'multiple' => false));
-        $builder->add('switch_checkboxes',  'switch',   array('label' => 'Switch (as checkboxes)', 'choices' => array(1 => 'Choice 1', 2 => 'Choice 2', 3 => 'Obi wan kenobi'), 'multiple' => true));
-
-        // Date and time
-        $builder->add('date',               'date',     array('label' => 'Date'));
-        $builder->add('time',               'time',     array('label' => 'Time'));
-        $builder->add('datetime',           'datetime', array('label' => 'Datetime'));
-
-        // Textarea
-        $builder->add('textarea',           'textarea', array('label' => 'Textarea'));
-
-
-        /*
-
-                $choices = array('choice a', 'choice b', 'choice c');
-        $form = $builder
+    {
+        $choices = array('Choice 1', 'Choice 2', 'Obi wan kenobi');
+        $builder
             ->add('text1', 'text', array(
                 'constraints' => new Assert\NotBlank(),
                 'attr' => array('placeholder' => 'not blank constraints')
@@ -74,13 +34,11 @@ class KitchensinkType extends AbstractType
                     ))
                     ->add('subformtext1', 'text')
             )
-
             ->add('number', 'number')
             ->add('password', 'password')
             ->add('percent', 'percent')
             ->add('search', 'search')
             ->add('url', 'url')
-
             ->add('choice1', 'choice', array(
                 'choices' => $choices,
                 'multiple' => true,
@@ -126,7 +84,6 @@ class KitchensinkType extends AbstractType
             ->add('radio', 'radio', array(
                 'constraints' => new Assert\True(),
             ))
-
             ->add('country', 'country')
             ->add('language', 'language')
             ->add('locale', 'locale')
@@ -169,17 +126,7 @@ class KitchensinkType extends AbstractType
                 'options' => array('required' => true),
                 'first_options' => array('label' => 'Password'),
                 'second_options' => array('label' => 'Repeat Password'),
-            ))
-            ->add('submit', 'submit', array('attr' => array('formnovalidate' => true)))
-
-         */
-    }
-
-    public function setDefaultOptions(OptionsResolverInterface $resolver)
-    {
-        $resolver->setDefaults(array(
-            'csrf_protection'   => false,
-        ));
+            ));
     }
 
     public function getName()
